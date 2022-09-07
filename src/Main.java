65b62f4ad34f0c1c3cda2c3601b2d4e4221c5c7a
aisalbek <110242974+aisalbek@users.noreply.github.com>
2022-09-07 15:44:00 +0600

    a

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Random random =new Random();
+        ArrayList<Integer>arrayList=new ArrayList<>();
+        LinkedList<Integer>linkedList=new LinkedList<>();
+        for (int i = 0; i < 10; i++) {
+            arrayList.add(i, random.nextInt(10));
+            linkedList.add(i, random.nextInt(10));
+        }
+        System.out.println("linkedlist  до сортировки :"+linkedList);
+        Collections.sort(linkedList);
+        System.out.println("linkedlist после сортировки :"+linkedList);
+        System.out.println("");
+        System.out.println("arrayList до сортировки :"+arrayList);
+        Collections.sort(arrayList);
+        System.out.println("arrayList после сортировки :"+arrayList);
+        }
+            }
