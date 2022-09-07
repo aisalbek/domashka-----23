8ac1280aef3277b14acea5461819f46d3fec7c05
aisalbek <110242974+aisalbek@users.noreply.github.com>
2022-09-07 17:12:48 +0600

    b

--- src/Main.java
+++ src/Main.java
@@ -12,17 +12,21 @@ public class Main {
 
         private static void Overload() {
           Random random = new Random();
+          int [] massiv=new int[10] ;
           ArrayList<Integer> arrayList = new ArrayList<>();
           LinkedList<Integer> linkedList = new LinkedList<>();
         for (int i = 0; i < 10; i++) {
+            massiv[i]= random.nextInt(2);
             arrayList.add(i, random.nextInt(2));
             linkedList.add(i, random.nextInt(2));
         }
-        System.out.println("linkedlist  до сортировки :" + linkedList);
+        System.out.println("massiv до сортировки"+Arrays.toString(massiv));
+        Arrays.sort(massiv);
+        System.out.println("massiv после сортировки"+Arrays.toString(massiv));
+        System.out.println("\nlinkedlist  до сортировки :" + linkedList);
         Collections.sort(linkedList);
         System.out.println("linkedlist после сортировки :" + linkedList);
-        System.out.println("");
-        System.out.println("arrayList до сортировки :" + arrayList);
+        System.out.println("\narrayList до сортировки :" + arrayList);
         Collections.sort(arrayList);
         System.out.println("arrayList после сортировки :" + arrayList);
     }
