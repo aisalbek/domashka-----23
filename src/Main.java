1158295cd19e041111762cd84c941a6e4f8e86cb
aisalbek <110242974+aisalbek@users.noreply.github.com>
2022-09-07 15:56:22 +0600

    b

--- src/Main.java
+++ src/Main.java
@@ -6,19 +6,24 @@ import java.util.LinkedList;
 public class Main {
     public static void main(String[] args) {
 
-        Random random =new Random();
-        ArrayList<Integer>arrayList=new ArrayList<>();
-        LinkedList<Integer>linkedList=new LinkedList<>();
+        Overload();
+    }
+
+
+        private static void Overload() {
+          Random random = new Random();
+          ArrayList<Integer> arrayList = new ArrayList<>();
+          LinkedList<Integer> linkedList = new LinkedList<>();
         for (int i = 0; i < 10; i++) {
-            arrayList.add(i, random.nextInt(10));
-            linkedList.add(i, random.nextInt(10));
+            arrayList.add(i, random.nextInt(2));
+            linkedList.add(i, random.nextInt(2));
         }
-        System.out.println("linkedlist  до сортировки :"+linkedList);
+        System.out.println("linkedlist  до сортировки :" + linkedList);
         Collections.sort(linkedList);
-        System.out.println("linkedlist после сортировки :"+linkedList);
+        System.out.println("linkedlist после сортировки :" + linkedList);
         System.out.println("");
-        System.out.println("arrayList до сортировки :"+arrayList);
+        System.out.println("arrayList до сортировки :" + arrayList);
         Collections.sort(arrayList);
-        System.out.println("arrayList после сортировки :"+arrayList);
-        }
-            }
+        System.out.println("arrayList после сортировки :" + arrayList);
+    }
+}
