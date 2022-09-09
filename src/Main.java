167584f9407083df9ec3381bd00d2fadf74ef87e
aisalbek <110242974+aisalbek@users.noreply.github.com>
2022-09-09 14:02:50 +0600

    imenenia

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,6 @@ import java.util.LinkedList;
 public class Main {
     public static void main(String[] args) {
 
-        Overload();
-    }
-
-
-        private static void Overload() {
           Random random = new Random();
           int [] massiv=new int[10] ;
           ArrayList<Integer> arrayList = new ArrayList<>();
@@ -21,13 +16,22 @@ public class Main {
             linkedList.add(i, random.nextInt(2));
         }
         System.out.println("massiv до сортировки"+Arrays.toString(massiv));
-        Arrays.sort(massiv);
-        System.out.println("massiv после сортировки"+Arrays.toString(massiv));
+        sorrt(massiv);
         System.out.println("\nlinkedlist  до сортировки :" + linkedList);
-        Collections.sort(linkedList);
-        System.out.println("linkedlist после сортировки :" + linkedList);
+        sorrt(linkedList);
         System.out.println("\narrayList до сортировки :" + arrayList);
+        sorrt(arrayList);
+    }
+    public static void sorrt(ArrayList<Integer>arrayList){
         Collections.sort(arrayList);
         System.out.println("arrayList после сортировки :" + arrayList);
+    }
+    public static void sorrt(LinkedList<Integer>linkedList){
+        Collections.sort(linkedList);
+        System.out.println("linkedlist после сортировки :" + linkedList);
+    }
+    public static void sorrt(int[]massiv){
+        Arrays.sort(massiv);
+        System.out.println("massiv после сортировки"+Arrays.toString(massiv));
     }
 }
